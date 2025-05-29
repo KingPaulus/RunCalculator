5c328c99d069a3d7e7dca5946275b46568deabbb
King Paulus <mergetp02@gmail.com>
2025-05-29 10:06:31 +0200

    Clean Up and Add Year 2025

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
@@ -24,9 +25,11 @@ public class Main {
 
     /**
      * Druckt die Namen und Laufzeiten sortiert nach der Laufzeit mit Platzierung.
+     *
      * @param results Ein 2D-Array, wobei jedes Element ein Array der Form {Name, Laufzeit} ist.
+     * @param year
      */
-    public static void printSortedResults(String[][] results) {
+    public static void printSortedResults(String[][] results, int year) {
         // Sortieren des Arrays basierend auf der umgewandelten Laufzeit in Sekunden
         Arrays.sort(results, (a, b) -> Integer.compare(convertTimeToSeconds(a[1]), convertTimeToSeconds(b[1])));
 
@@ -38,55 +41,29 @@ public class Main {
         }
     }
 
+    /**
+     * Main Methode zum starten der Anwendung
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        // Beispiel für ein 2D-Array mit Namen und Laufzeiten im Format hh:mm:ss
-        String[][] runners = {
-                {"Arweiler, Klaus ", "00:32:07"},
-                {"Bötsch, Christina ", "00:36:52"},
-                {"Brux, Tobias ", "00:27:22"},
-                {"Dellermann, Daniela ", "00:47:23"},
-                {"Diemer, Valeriya ", "00:39:15"},
-                {"Dinkel, Frank ", "00:39:25"},
-                {"Doan, Hang ", "00:50:44"},
-                {"Freitag, Matthias ", "00:34:48"},
-                {"Fuss, Christian ", "00:31:38"},
-                {"Geißendörfer, Markus ", "00:34:59"},
-                {"Hehn, Albert ", "00:44:16"},
-                {"Heinz, Anton ", "00:45:02"},
-                {"Hüller, Sebastian ", "00:25:03"},
-                {"Klein, Clemens ", "00:35:28"},
-                {"Kohler, Jörg ", "00:39:19"},
-                {"Kriester, Uwe ", "00:27:12"},
-                {"Lukoschek, Kester ", "00:36:37"},
-                {"Lurz, Frank ", "00:27:32"},
-                {"Mählich, Robert ", "00:43:43"},
-                {"Manke, Maik ", "00:32:34"},
-                {"Manske, Julian ", "00:25:38"},
-                {"Merget, Paul ", "00:32:13"},
-                {"Nangue Tankoua, Elodie ", "00:50:36"},
-                {"Pfister, Jens ", "00:33:12"},
-                {"Plume, Monika ", "00:39:18"},
-                {"Pöhler, Simone ", "00:41:22"},
-                {"Reichert, Johannes ", "00:39:04"},
-                {"Rincker, Yannik ", ""},
-                {"Schadewaldt, Joshua ", "00:31:45"},
-                {"Schmäling, Uwe ", "00:25:29"},
-                {"Schraud, Stefan ", "00:31:22"},
-                {"Schuck, Julian ", "00:29:13"},
-                {"Schuler, Karin ", "00:39:15"},
-                {"Schulze, Karsten ", "00:30:50"},
-                {"Schumacher, Kristian ", "00:36:46"},
-                {"Stelter, Benjamin ", "00:48:44"},
-                {"Stürmer, Jan ", "00:53:16"},
-                {"Teister, Damian ", "00:39:33"},
-                {"Thomae, Enrico ", "00:22:53"},
-                {"Veeh, Johannes ", "00:32:33"},
-                {"Weber, Sita ", "00:45:17"},
-                {"Wendel, Fabian ", "00:39:24"},
-                {"Zimmermann, Mandy ", "00:52:42"},
-        };
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bitte Jahr eingeben (z. B. 2024 oder 2025): ");
+        int year = scanner.nextInt();
 
-        // Funktion aufrufen, um Ergebnisse zu drucken
-        printSortedResults(runners);
+        String[][] runners;
+        switch (year) {
+            case 2024:
+                runners = Data2024.getRunners();
+                break;
+            case 2025:
+                runners = Data2025.getRunners();
+                break;
+            default:
+                System.out.println("Kein Datensatz für das Jahr " + year + " gefunden.");
+                return;
+        }
+
+        printSortedResults(runners, year);
     }
 }
