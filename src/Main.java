0af766e1be4c0ea8f22e715437b9be73d1563428
King Paulus <mergetp02@gmail.com>
2026-06-19 09:26:51 +0200

    Add 2026 to the Calculator

--- src/Main.java
+++ src/Main.java
@@ -48,7 +48,7 @@ public class Main {
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Bitte Jahr eingeben (z. B. 2024 oder 2025): ");
+        System.out.print("Bitte Jahr eingeben (2024, 2025, 2026): ");
         int year = scanner.nextInt();
 
         String[][] runners;
@@ -58,6 +58,9 @@ public class Main {
                 break;
             case 2025:
                 runners = Data2025.getRunners();
+                break;
+            case 2026:
+                runners = Data2026.getRunners();
                 break;
             default:
                 System.out.println("Kein Datensatz für das Jahr " + year + " gefunden.");
